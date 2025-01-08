58ecfd729295c7078d0faa5089fc7484041f7311
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-08 14:06:14 -0500

    Add associations between Post and User models
    - Added `belongs_to :author` association to the Post model
    (using `User` as the class).
    - Added `has_many :posts` association to the User model.
    - Renamed the foreign key in the posts table to `author_id` to
    represent the association.
    - Updated migrations to include a foreign key constraint on `author_id`
    referencing the users table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Establish bi-directional association with Post model
+  has_many :posts, inverse_of: "author", foreign_key: "author_id"
 end
