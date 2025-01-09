d944c0fecb6aceab9da8360e3c950c61712e3b91
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-09 15:49:09 -0500

    git commit -m "Add username to signup form - Updated Devise signup form to include username field - Created HomeController to manage user sign-in and sign-out - HomeController handles rendering of sign-in and sign-out views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   self.responder = ApplicationResponder
   respond_to :html
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
+  end
 end
